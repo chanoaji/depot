d058aee3758fb2768a1fa3b7c2883f8fa0c53af6
Vanessa Pasquet <pasquet.vanessa@gmail.com>
2012-10-19 13:47:12 +0200

    change of minimium length for the title attribute of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,9 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true
+	validates :title, :length => {:minimum => 10}
 	validates :image_url, :format =>{
-		:with => %r{\.(gof|jpg|png)$}i,
+		:with => %r{\.(gif|jpg|png)$}i,
 		:message => 'must be a URL for GIF, JPG or PNG image.'
 	}
   attr_accessible :title, :description, :image_url, :price
